5dfa5ecdace82ad6b8afcd24d17513050303bd4e
Sabrina Gelbart <sabrina.gelbart@gmail.com>
2016-08-11 19:09:25 -0500

    tinkerin' trying to get the router to work. Also added check for logged in

--- both/router.js
+++ both/router.js
@@ -3,9 +3,15 @@ Router.configure({
 });
 Router.route('/', 'items');
 Router.route('/users','users');
-Router.route('/items/:_id', function () {
-    var item = Items.findOne({_id: this.params._id});
-    this.render('item', {data: item});
-}, {
-    name: 'items.show'
-});
+//Router.route('/items/:_id', function () {
+//    var item = Items.findOne({_id: this.params._id});
+//    this.render('item', {data: item});
+//}, {
+//    name: 'items.show'
+//});
+//
+//Router.route('/', function () {
+//    this.render('Home', {
+//        data: function () { return Items.findOne({_id: this.params._id}); }
+//    });
+//});
